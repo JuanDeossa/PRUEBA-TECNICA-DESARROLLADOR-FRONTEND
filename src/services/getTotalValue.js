8f0b956d3340b09424b4b79e704b102f4b339682
JuanDeossa <juandeossa.nc@gmail.com>
2023-01-20 04:08:37 -0500

    getTotalValue.js service ok

---
+++ src/services/getTotalValue.js
@@ -0,0 +1,7 @@
+export const getTotalValue = (array) => {
+  const total = array
+    ?.map((product) => product?.price)
+    .reduce((price1, price2) => price1 + price2);
+  return total.toFixed(2);
+};
+
