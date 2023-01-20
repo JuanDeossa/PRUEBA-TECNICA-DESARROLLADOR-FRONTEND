ae55780594e45378c9e7700d9fba2499d7d7dfaa
JuanDeossa <juandeossa.nc@gmail.com>
2023-01-20 04:37:22 -0500

    getTotalValue.js service bug fixed

--- src/services/getTotalValue.js
+++ src/services/getTotalValue.js
@@ -1,6 +1,6 @@
 export const getTotalValue = (array) => {
   const total = array
-    ?.map((product) => product?.price)
+    ?.map((product) => product?.price * product?.units)
     .reduce((price1, price2) => price1 + price2, 0);
   return total.toFixed(2);
 };
